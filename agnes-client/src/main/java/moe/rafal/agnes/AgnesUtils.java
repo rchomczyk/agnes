664f8e60b99ae2c5ae94346a928218ea21c1aedc
Rafał Chomczyk <hello@rafal.moe>
2024-04-01 19:34:54 +0200

    Update copyright notice

--- agnes-client/src/main/java/moe/rafal/agnes/AgnesUtils.java
+++ agnes-client/src/main/java/moe/rafal/agnes/AgnesUtils.java
@@ -1,5 +1,5 @@
 /*
- *    Copyright 2023 agnes
+ *    Copyright 2023-2024 agnes
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
